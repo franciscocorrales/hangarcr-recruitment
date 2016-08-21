8835782024bc6a62e6e4857b2d210c587ae0808f
franciscocorrales <fcm200837354@gmail.com>
2016-08-21 15:50:30 -0600

    Update db seeder

--- database/seeds/SongsTableSeeder.php
+++ database/seeds/SongsTableSeeder.php
@@ -7,13 +7,73 @@ class SongsTableSeeder extends Seeder {
         DB::table('songs')->delete();
  
         User::create(array(
-			'id' => '25479197',
-            'url' => 'spotify:album:3qfz9wig4gcrb4bimw9ov7',
+			'id' => 2547919',
+            'url' => 'spotify:track:3qfz9wig4gcrb4bimw9ov7',
             'songname' => 'johnny b. goode',
-            'artistid' => '45',
+            'artistid' => 45,
             'artistname' => 'chuck berry',
-            'albumid' => '235469',
+            'albumid' => 235469,
             'albumname' => 'roll over beethoven'
+        ));
+                
+        User::create(array(
+			'id' => 881558,
+            'url' => 'spotify:track:7linrtr5px7i3r96mducjw',
+            'songname' => 'moonlight sonata',
+            'artistid' => 1830,
+            'artistname' => 'beethoven',
+            'albumid' => 5619520,
+            'albumname' => 'beethoven piano sona'
+        ));
+        
+         User::create(array(
+			'id' => 8815586,
+            'url' => 'spotify:track:7linrtr5px7i3r96mducjw',
+            'songname' => 'twist and shout',
+            'artistid' => 1833,
+            'artistname' => 'beatles',
+            'albumid' => 5619520,
+            'albumname' => 'beatles'
+        ));
+        
+         User::create(array(
+			'id' => 8815587,
+            'url' => 'spotify:track:7linrtr5px7i3r96mducjw',
+            'songname' => 'i saw her standing there',
+            'artistid' => 1833,
+            'artistname' => 'beatles',
+            'albumid' => 5619520,
+            'albumname' => 'beatles'
+        ));
+        
+         User::create(array(
+			'id' => 8815588,
+            'url' => 'spotify:track:7linrtr5px7i3r96mducjw',
+            'songname' => 'here comes the sun',
+            'artistid' => 1833,
+            'artistname' => 'beatles',
+            'albumid' => 5619520,
+            'albumname' => 'beatles'
+        ));
+                
+         User::create(array(
+			'id' => 8815589,
+            'url' => 'spotify:track:7linrtr5px7i3r96mducjw',
+            'songname' => 'hey jude',
+            'artistid' => 1833,
+            'artistname' => 'beatles',
+            'albumid' => 5619520,
+            'albumname' => 'beatle'
+        ));
+        
+        User::create(array(
+			'id' => 8815590,
+            'url' => 'spotify:track:7linrtr5px7i3r96mducjw',
+            'songname' => 'helter skelter',
+            'artistid' => 1833,
+            'artistname' => 'beatles',
+            'albumid' => 5619520,
+            'albumname' => 'beatles'
         ));
  
     }
