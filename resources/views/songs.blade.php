919e0ec6a9f54831beb55ee6eab5cedfea95909a
franciscocorrales <fcm200837354@gmail.com>
2016-08-22 02:28:56 -0600

    Better index page view.

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -38,8 +38,16 @@
 			}
 			
 			.songs p{
-				float: right;
-			}	
+				margin: 0;
+			}
+			
+			.songs label {
+				width: 100px;
+			}
+			
+			.songs .song{
+				margin-bottom: 12px;
+			}
         </style>
     </head>
     <body>
@@ -54,15 +62,17 @@
 						<h1>Songs currently on the API:</h1>
 						
 						@foreach($songs as $song)
-							<div><label>id:</label><p>{{$song['attributes']['id']}}</p></div>
-							<div><label>url:</label><p>{{$song['attributes']['url']}}</p></div>
-							<div><label>songname:</label><p>{{$song['attributes']['songname']}}</p></div>
-							<div><label>artistid:</label><p>{{$song['attributes']['artistid']}}</p></div>
-							<div><label>artistname:</label><p>{{$song['attributes']['artistname']}}</p></div>
-							<div><label>albumid:</label><p>{{$song['attributes']['albumid']}}</p></div>
-							<div><label>albumname:</label><p>{{$song['attributes']['albumname']}}</p></div>
-							<div><label>created_at:</label><p>{{$song['attributes']['created_at']}}</p></div>
-							<div><label>updated_at:</label><p>{{$song['attributes']['updated_at']}}</p></div>
+							<div class="song">
+								<div><label>id:</label><p>{{$song['attributes']['id']}}</p></div>
+								<div><label>url:</label><p>{{$song['attributes']['url']}}</p></div>
+								<div><label>songname:</label><p>{{$song['attributes']['songname']}}</p></div>
+								<div><label>artistid:</label><p>{{$song['attributes']['artistid']}}</p></div>
+								<div><label>artistname:</label><p>{{$song['attributes']['artistname']}}</p></div>
+								<div><label>albumid:</label><p>{{$song['attributes']['albumid']}}</p></div>
+								<div><label>albumname:</label><p>{{$song['attributes']['albumname']}}</p></div>
+								<div><label>created_at:</label><p>{{$song['attributes']['created_at']}}</p></div>
+								<div><label>updated_at:</label><p>{{$song['attributes']['updated_at']}}</p></div>
+							</div>
 						@endforeach
 						
 					@endif
