55658a975893712dd8d6ff02b3a991c0db8669f0
franciscocorrales <fcm200837354@gmail.com>
2016-08-22 13:17:42 -0600

    Start using Dingo API.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,12 @@
 |
 */
 
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+	//$api->get('users/{id}', 'App\Api\Controllers\UserController@show');
+});
+
 Route::get('/', 'SongsController@index');
 
 Route::get('/api/v1/songs', 'SongsController@all');
