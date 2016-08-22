c4ab81d17f8e6f7b208e994705fd16095ffc362d
franciscocorrales <fcm200837354@gmail.com>
2016-08-22 13:25:22 -0600

    Switch routes to Dingo.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,15 +14,19 @@
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
-	//$api->get('users/{id}', 'App\Api\Controllers\UserController@show');
+	$api->get('songs', 'App\Http\Controllers\SongsController@all');
+	$api->get('songs/{id}', 'App\Http\Controllers\SongsController@show');
+	$api->post('songs', 'App\Http\Controllers\SongsController@store');
+	$api->put('songs/{id}', 'App\Http\Controllers\SongsController@update');
+	$api->delete('songs/{id}', 'App\Http\Controllers\SongsController@destroy');
 });
 
 Route::get('/', 'SongsController@index');
 
-Route::get('/api/v1/songs', 'SongsController@all');
-Route::get('/api/v1/songs/{id}', 'SongsController@show');
-Route::post('/api/v1/songs', 'SongsController@store');
-Route::put('/api/v1/songs/{id}', 'SongsController@update');
-Route::delete('/api/v1/songs/{id}', 'SongsController@destroy');
+//Route::get('/api/v1/songs', 'SongsController@all');
+//Route::get('/api/v1/songs/{id}', 'SongsController@show');
+//Route::post('/api/v1/songs', 'SongsController@store');
+//Route::put('/api/v1/songs/{id}', 'SongsController@update');
+//Route::delete('/api/v1/songs/{id}', 'SongsController@destroy');
 
 Route::any('/{any}', 'SongsController@index')->where('any', '.*');
