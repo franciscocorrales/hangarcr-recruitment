50d23d93d9d7d7b661ecd8bec4ad9adfeb06bb25
franciscocorrales <fcm200837354@gmail.com>
2016-08-21 19:49:05 -0600

    Use Model.

--- app/Http/Controllers/SongsController.php
+++ app/Http/Controllers/SongsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Song;
+
+
 
 class SongsController extends Controller
 {
@@ -56,6 +58,7 @@ class SongsController extends Controller
     public function index()
     {
 		$songs = $this->getJSONSongs();
+		$songs = Song::all();
         return view('songs', compact('songs'));
     }
     
